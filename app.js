76986b9c806b0d298a8ec9b39fe865ee881b7ab6
Imran <ihkldoo@gmail.com>
2022-03-15 14:30:26 +0300

    make it better

--- app.js
+++ app.js
@@ -9,13 +9,12 @@ dotenv.config({ path: path.join(__dirname, "config", "config.env") });
 const app = express();
 app.use(express.json());
 
-const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["POST", "GET"],
+  })
+);
 app.use(morgran("dev"));
 
 readdirSync(path.join(__dirname, "routes")).map((route) =>
