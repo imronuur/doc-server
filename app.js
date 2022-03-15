ca087454562f8388d12fab3e21f22d8ac8959c22
Imran <ihkldoo@gmail.com>
2022-03-15 14:40:54 +0300

    make it better

--- app.js
+++ app.js
@@ -9,13 +9,8 @@ dotenv.config({ path: path.join(__dirname, "config", "config.env") });
 const app = express();
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "*",
-    methods: ["POST", "GET"],
-  })
-);
-app.use(morgran("dev"));
+app.use(cors());
+app.use(morgran("combined"));
 
 readdirSync(path.join(__dirname, "routes")).map((route) =>
   app.use("/api", require(path.join(__dirname, "routes/" + route)))
